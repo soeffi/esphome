37b372fb5c4c25acf94344667a6a65b3a3e085f6
soeffi <soeffing@gmx.de>
2024-07-04 23:43:19 +0200

    Update jsn_sr04t.cpp
    Calculate checksum according to selected chip

--- esphome/components/jsn_sr04t/jsn_sr04t.cpp
+++ esphome/components/jsn_sr04t/jsn_sr04t.cpp
@@ -31,8 +31,10 @@ void Jsnsr04tComponent::loop() {
 }
 
 void Jsnsr04tComponent::check_buffer_() {
-  //uint8_t checksum = this->buffer_[0] + this->buffer_[1] + this->buffer_[2];
   uint8_t checksum = this->buffer_[1] + this->buffer_[2];
+  if(!ajsr04m)
+    checksum += this->buffer_[0];
+  
   if (this->buffer_[3] == checksum) {
     uint16_t distance = encode_uint16(this->buffer_[1], this->buffer_[2]);
     if (distance > 250) {
@@ -50,6 +52,7 @@ void Jsnsr04tComponent::check_buffer_() {
 
 void Jsnsr04tComponent::dump_config() {
   LOG_SENSOR("", "JST_SR04T Sensor", this);
+  ESP_LOGCONFIG(TAG, "  checksum mode: %s", this->ajsr04m_ ? "aj_sr04m" : "jsn_sr04t");
   LOG_UPDATE_INTERVAL(this);
 }
 
